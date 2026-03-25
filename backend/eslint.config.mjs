f3173ff6c39b0a472ddcd3acf071eb52bd2a1aa8
Milan Magyar <milan.magyar@gmail.com>
2026-03-25 09:01:39 +0100

    Add eslint for backend

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,14 @@
+import baseConfig from '@hono/eslint-config'
+
+export default [
+  ...baseConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.mjs']
+        }
+      }
+    }
+  }
+]
